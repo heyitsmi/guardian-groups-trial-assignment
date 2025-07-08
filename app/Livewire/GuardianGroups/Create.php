fae752feb293d1e8166d55fe49cd8b5ec9f98bc4
Hilmi Hidayat <hilmihidayat175@gmail.com>
2025-07-08 22:44:52 +0700

    implement dynamic title

---
+++ app/Livewire/GuardianGroups/Create.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\GuardianGroups;
+
+use Livewire\Component;
+
+class Create extends Component
+{
+    public function render()
+    {
+        return view('livewire.guardian-groups.create');
+    }
+}
--- app/Livewire/Home.php
+++ app/Livewire/Home.php
@@ -3,11 +3,10 @@
 namespace App\Livewire;
 
 use Livewire\Component;
-
+use Livewire\Attributes\Title;
 class Home extends Component
 {
-    public $title = 'Home';
-    
+    #[Title('Home')] 
     public function render()
     {
         return view('livewire.home');
